c530b098f0e87bad7a87e72f27272395931227f7
xiaobxia <673806687@qq.com>
2019-03-30 09:21:13 +0800

    xg

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     // assetsPublicPath: './',
     assetsPublicPath: './',
     //放七牛加速，减少带宽占用
-    cdnPublicPath: 'http://pnmjmpq0j.bkt.clouddn.com/',
+    cdnPublicPath: 'http://pp5n1dym0.bkt.clouddn.com/',
     //打包就移除sourceMap
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
