7f09a42381be2d5cba3ebcb6d4fc841d608a56b7
xiaobxia <673806687@qq.com>
2020-06-22 15:51:31 +0800

    xg

--- src/views/fixChart/index.vue
+++ src/views/fixChart/index.vue
@@ -16,11 +16,11 @@
 <script>
 import indexList from '@/common/indexList'
 import arrayUtil from '@/utils/arrayUtil'
-import stockAnalysisUtil from '@/utils/stockAnalysisUtil'
 import echarts from 'echarts'
+import moment from 'moment'
 
 export default {
-  name: 'MonthLineAll',
+  name: 'FixChart',
   data() {
     return {
       indexKey: 'chuangye',
@@ -28,7 +28,8 @@ export default {
       dataList: [],
       chart: null,
       indexItem: null,
-      id: 'MonthLineAll'
+      id: 'FixChart',
+      signList: []
     }
   },
   computed: {
@@ -39,7 +40,9 @@ export default {
     }
   },
   mounted() {
-    this.initPage()
+    this.querySign().then(() => {
+      this.initPage()
+    })
   },
   beforeDestroy() {
     if (!this.chart) {
@@ -62,15 +65,23 @@ export default {
         }
       }
     },
+    querySign() {
+      return this.$http.get('http://47.92.210.171:3051/fbsServer/signal/getSignalsByDays', {
+        days: 20
+      }).then((res) => {
+        this.signList = res.data || []
+      })
+    },
     initPage() {
       const indexItem = arrayUtil.findItem(indexList, 'key', this.indexKey)
+      this.indexItem = indexItem
       if (!indexItem) {
         return
       }
       this.indexItem = indexItem
       this.$http.get('stock/getStockAllDongfang', {
         code: indexItem.code,
-        days: indexItem.days
+        days: 20
       }).then((res) => {
         const list = []
         res.data.list.forEach((item) => {
@@ -83,146 +94,42 @@ export default {
         this.initChart()
       })
     },
+    getSign(date, key) {
+      date = moment(date).format('YYYY-MM-DD')
+      for (let i = 0; i < this.signList.length; i++) {
+        const item = this.signList[i]
+        if (item.trade_date === date) {
+          const bandRecord = item.band_record || []
+          for (let j = 0; j < bandRecord.length; j++) {
+            const bandItem = bandRecord[j]
+            if (bandItem.key === key) {
+              return bandItem.flag
+            }
+          }
+        }
+      }
+      return ''
+    },
     initChart() {
       this.chart = echarts.init(document.getElementById(this.id))
-      const indexRate = this.indexItem.rate
       const recentNetValue = this.dataList
-      // 均线
-      const recentNetValue2 = this.$getAverageList(recentNetValue, 20)
-      const recentNetValue3 = this.$getAverageList(recentNetValue, 120)
       const xData = []
       const yData = []
-      const yData2 = []
-      const yData3 = []
       const points = []
+      // 最近的在前面
       const netChangeRatioAll = []
-      recentNetValue2.forEach((item) => {
-        yData2.push(item)
-      })
-      recentNetValue3.forEach((item) => {
-        yData3.push(item)
-      })
       recentNetValue.forEach((item, index) => {
         xData.unshift(item['date'])
         yData.unshift(item['close'])
+        const sign = this.getSign(item['date'], this.indexItem.key)
+        if (sign) {
+          if (sign.indexOf('加') !== -1) {
+            points.push(this.createPoint(item['date'], item['close'], 'red'))
+          } else if (sign.indexOf('减') !== -1) {
+            points.push(this.createPoint(item['date'], item['close'], 'green'))
+          }
+        }
         netChangeRatioAll.push(item.netChangeRatio)
-      })
-      // 线差值
-      const yData4 = []
-      yData.forEach((item, index) => {
-        const day = 5
-        const averageList = []
-        if (index >= day) {
-          for (let i = day; i >= 0; i--) {
-            const nowIndex = index - i
-            averageList.push(this.$countDifferenceRate(yData[nowIndex], yData2[nowIndex]))
-          }
-        }
-        const noSell = stockAnalysisUtil.ifNoSell(averageList)
-        const rate = this.$countDifferenceRate(item, yData2[index])
-        // 移动均线策略
-        let now = 0
-        let last = 0
-        let c = true
-        // 近的在前
-        for (let i = 0; i < 7; i++) {
-          if (yData[index - i]) {
-            now += parseFloat(yData[index - i])
-          }
-        }
-        for (let j = 1; j < 8; j++) {
-          if (yData[index - j]) {
-            last += parseFloat(yData[index - j])
-          }
-        }
-        const diff = this.$countDifferenceRate(now / 7, last / 7)
-        if (diff < 0.2) {
-          c = false
-        }
-        yData4.push({
-          value: rate,
-          itemStyle: {
-            color: noSell ? (c ? 'rgb(208, 153, 183)' : 'rgb(0, 0, 0)') : 'rgb(112, 220, 240)'
-          }
-        })
-      })
-      const allLength = netChangeRatioAll.length
-      // 画点
-      netChangeRatioAll.forEach((item, index) => {
-        // 最后几个不要
-        if (allLength - 10 <= index) {
-          return false
-        }
-        const show = true
-        const nowKline = recentNetValue[index]
-        const netChangeRatioList = this.$getNetChangeRatioList(netChangeRatioAll, index)
-        const threeDay = stockAnalysisUtil.countDown(netChangeRatioList, 3, 3)
-        const date = nowKline['date']
-        const upValue = nowKline['close'] + (nowKline['close'] / 40)
-        const downValue = nowKline['close'] - (nowKline['close'] / 100)
-        if (threeDay.flag && threeDay.rate < -(3 * indexRate)) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, '#ff0000'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 6, 5).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, '#ff0000'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 4, 4).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, '#ff0000'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 5, 5).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, 'black'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 7, 6).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, 'black'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 8, 7).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, 'black'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 8, 6).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, 'black'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countDown(netChangeRatioList, 9, 7).flag) {
-          if (show) {
-            points.push(this.createPoint(date, downValue, 'black'))
-          } else {
-            return false
-          }
-        }
-        if (stockAnalysisUtil.countLowDown(netChangeRatioList).flag) {
-          if (show) {
-            points.push(this.createPoint(date, upValue, 'green'))
-          } else {
-            return false
-          }
-        }
       })
       this.chart.setOption({
         title: {
@@ -255,13 +162,6 @@ export default {
             splitLine: {
               show: false
             }
-          },
-          {
-            type: 'value',
-            name: '差值',
-            splitLine: {
-              show: false
-            }
           }
         ],
         series: [
@@ -277,42 +177,7 @@ export default {
             markPoint: {
               data: points,
               symbol: 'circle',
-              symbolSize: 4
-            }
-          },
-          {
-            name: '月线',
-            data: yData2,
-            type: 'line',
-            lineStyle: {
-              color: 'rgb(132, 7, 189)'
-            },
-            smooth: false,
-            symbol: 'none'
-          },
-          {
-            name: '半年线',
-            data: yData3,
-            type: 'line',
-            lineStyle: {
-              color: '#a80'
-            },
-            smooth: false,
-            symbol: 'none'
-          },
-          {
-            name: '差值',
-            data: yData4,
-            yAxisIndex: 1,
-            type: 'bar',
-            markLine: {
-              silent: true,
-              data: [{
-                yAxis: this.indexItem.reduceLine,
-                lineStyle: {
-                  color: '#F56C6C'
-                }
-              }]
+              symbolSize: 8
             }
           }
         ]
